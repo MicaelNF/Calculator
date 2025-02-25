3cdccfe3ba896715393a1bd4f62c5adaabddbe0a
MicaelNF <micaelnf@ciencias.unam.mx>
2025-02-25 00:21:56 -0600

    The Api for the Operations was made.

---
+++ CalculatorApp/src/main/java/Calculator/Operations.java
@@ -0,0 +1,12 @@
+package Calculator;
+
+public interface Operations {
+
+    double addition(Object a, Object b);
+
+    double subtract(Object a, Object b);
+
+    double multiply(Object a, Object b);
+
+    double divide(Object a, Object b);
+}
